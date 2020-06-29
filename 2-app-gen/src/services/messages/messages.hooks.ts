68369f082368692c87e15d207ce92aaf2edddba0
Zachary Cauchi <zach.cauchi@hotmail.com>
2020-06-29 17:06:28 +0200

    add hooks for handling users

--- 2-app-gen/src/services/messages/messages.hooks.ts
+++ 2-app-gen/src/services/messages/messages.hooks.ts
@@ -1,21 +1,32 @@
 import * as authentication from '@feathersjs/authentication';
+import { HookContext } from '@feathersjs/feathers';
+import processMessage from '../../hooks/process-message';
+import populateUser from '../../hooks/populate-user';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
+
+const setTimestamp = (name: string) => {
+  return async (context: HookContext) => {
+    context.data[name] = new Date();
+
+    return context;
+  }
+}
 
 export default {
   before: {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
-    create: [],
-    update: [],
+    create: [setTimestamp('createdAt'), processMessage()],
+    update: [ setTimestamp('updatedAt') ],
     patch: [],
     remove: []
   },
 
   after: {
-    all: [],
+    all: [populateUser()],
     find: [],
     get: [],
     create: [],
